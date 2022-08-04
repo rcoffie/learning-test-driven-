a568a8e1e7d77fa97ceb4446024111217fbe819d
Robert <rcoffie22@yahoo.com>
2022-08-04 15:27:53 +0000

    ending todays lessons on testiong

---
+++ solos/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from solos.models import Solo
+
+
+class SoloModelTestCase(TestCase):
+    def setUp(self):
+        self.solo = Solo.objects.create(
+        track='Falling in Love with Love',
+        artist = 'Oscar Peterson',
+        instrument = 'piano'
+
+        )
+
+    def test_solo_basic(self):
+        self.assertEqual(self.solo.artist, 'Oscar Peterson')
